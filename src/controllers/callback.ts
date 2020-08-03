a89a0f94fd252d5a6a8152d0c85cce86d3a5e792
AlanGuo <AlanGuo>
2020-08-03 11:52:12 +0800

    update

--- src/controllers/callback.ts
+++ src/controllers/callback.ts
@@ -1,13 +1,13 @@
 import { Controller, POST } from "@src/infrastructure/decorators/koa";
 import { IRouterContext } from "koa-router";
 
+
+// todo: need to verity the signatures of Amazon SNS messages
 @Controller({ prefix: "/callback" })
 export default class CallbackController {
   @POST("/mediaconvertcomplete/notification")
   async notify(ctx: IRouterContext) {
-    const req = ctx.request;
     const body = ctx.request.body;
-    console.log(req, body);
-    ctx.body = {};
+    console.log(body.MessageId, body.Type, body.Message, body.TopicArn, body.Timestamp, body.MessageAttributes);
   }
 }
