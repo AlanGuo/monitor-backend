536c21ccbcf2a86bfe85ef2dede9815193d4e802
AlanGuo <AlanGuo>
2020-08-17 10:28:09 +0800

    update

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,13 +3,13 @@ import KoaRouter, {IRouterContext} from "koa-router";
 import UserModel from "../models/user"
 import { jsonResponse, unauthorized } from "@src/infrastructure/utils";
 import {RESPONSE_CODE} from "@src/infrastructure/utils/constants";
-import { LoginRequired } from "@src/infrastructure/decorators/auth";
+import { AuthRequired } from "@src/infrastructure/decorators/auth";
 
 @Controller({prefix: "/users"})
 export default class UserController {
 
   @GET()
-  // @AuthRequired()
+  @AuthRequired()
   async getUsers(ctx: IRouterContext, next: any) {
     const fields = {_id: 0, uuid: 1, displayName: 1, avatar: 1, email: 1};
     const users = await UserModel.find({}, fields);
