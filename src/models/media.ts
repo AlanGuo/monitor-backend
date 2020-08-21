03de28f8cca71758cf075914cecba2828f9b0238
hffp <510033939@qq.com>
2020-08-21 23:53:31 +0800

    update

--- src/models/media.ts
+++ src/models/media.ts
@@ -1,19 +1,18 @@
 import {Schema, Types, model, Document} from "mongoose";
 import {MEDIA_TYPE} from "@src/infrastructure/utils/constants";
-import {BigNumber} from "bignumber.js";
 
 const required = true;
 
 export interface Media extends Document {
   type: MEDIA_TYPE,
-  owner: Types.ObjectId,
+  owner: number,
   fileName: string,
   price?: string;
 }
 
 const MediaModel: Schema = new Schema({
   type: {type: MEDIA_TYPE, required},
-  owner: {type: Schema.Types.ObjectId, required},
+  owner: {type: Number, required},
   fileName: {type: String, required},
   price: {type: String, required, default: '0'}
 }, {
