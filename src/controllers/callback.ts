6c97367b0d2d9db882e11a76451351675a99e3c6
hffp <510033939@qq.com>
2020-08-27 17:01:47 +0800

    update

--- src/controllers/callback.ts
+++ src/controllers/callback.ts
@@ -4,7 +4,7 @@ import {Controller, POST} from "@src/infrastructure/decorators/koa";
 import {IRouterContext} from "koa-router";
 import {getOnlineUser, redis} from "@src/infrastructure/redis";
 import {getSocketIO} from "@src/infrastructure/socket";
-import {MEDIA_TYPE, SOCKET_CHANNEL} from "@src/infrastructure/utils/constants";
+import {MEDIA_PURPOSE, MEDIA_TYPE, SOCKET_CHANNEL} from "@src/infrastructure/utils/constants";
 import {jsonResponse} from "@src/infrastructure/utils/helper";
 import {isImage} from "@src/infrastructure/utils/image";
 import {isVideo} from "@src/infrastructure/utils/video";
@@ -14,14 +14,14 @@ import {ImageAmazonUrl, VideoAmazonUrl} from "@src/interface";
 
 
 // todo: need to verity the signatures of Amazon SNS messages
-@Controller({ prefix: "/callback" })
+@Controller({prefix: "/callback"})
 export default class CallbackController {
   @POST("/mediaconvertcomplete/notification")
   async notify(ctx: IRouterContext) {
     const body = ctx.request.body;
     const message = JSON.parse(body.Message);
     const records = message.Records;
-    for(let recordItem of records){
+    for (let recordItem of records) {
       const fileName = recordItem.s3.object.key;
       const ext = fileName.split(".")[1];
       let redisKey = fileName.split(".")[0];
@@ -32,20 +32,17 @@ export default class CallbackController {
       if (data) {
         const decodedData = JSON.parse(data);
         if (decodedData.fileCount > 1) {
-          decodedData.fileCount --;
+          decodedData.fileCount--;
           await redis.set(redisKey, JSON.stringify(decodedData));
         } else {
           // all files have been converted successfully
-          // 这里需要根据类型入库
-          // 转换成功发送给订阅者
-          // TODO publish to MQ
           if (decodedData.subscribers.length) {
             const io = getSocketIO();
             if (isImage(ext)) {
               const fileName = decodedData.key.replace(config.AWS_MEDIA_CONVERT.imageSourceFolder, "");
               await redis.del(redisKey);
               const urls = (getMediaUrl(MEDIA_TYPE.IMAGE, fileName) as ImageAmazonUrl);
-              for(let uuid of decodedData.subscribers) {
+              for (let uuid of decodedData.subscribers) {
                 const msg = JSON.stringify({
                   type: MEDIA_TYPE.IMAGE,
                   key: decodedData.key,
@@ -59,11 +56,11 @@ export default class CallbackController {
                   io.sockets.connected[sid].emit(SOCKET_CHANNEL.MEDIA_CONVERTED, msg);
                 }
               }
-            } else if(isVideo(ext)) {
+            } else if (isVideo(ext)) {
               const fileNameWithoutExt = decodedData.key.split(".")[0].replace(config.AWS_MEDIA_CONVERT.videoSourceFolder, "");
               await redis.del(redisKey);
-              for(let uuid of decodedData.subscribers){
-                const urls = getMediaUrl(MEDIA_TYPE.VIDEO, fileNameWithoutExt) as VideoAmazonUrl;
+              for (let uuid of decodedData.subscribers) {
+                const urls = getMediaUrl(MEDIA_TYPE.VIDEO, fileNameWithoutExt, MEDIA_PURPOSE.CHAT) as VideoAmazonUrl;
                 const msg = JSON.stringify({
                   type: MEDIA_TYPE.VIDEO,
                   key: decodedData.key,
