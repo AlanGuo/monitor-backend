7067c3ea1740d22893c40d00983e9e05207c7fa1
hffp <510033939@qq.com>
2020-09-25 15:55:37 +0800

    recommend

--- src/controllers/recommendUser.ts
+++ src/controllers/recommendUser.ts
@@ -36,11 +36,47 @@ export default class RecommendUser {
       },
       fields
     ])
-    recommend.forEach(item => {item.user = item.user[0]})
+    recommend.forEach(item => {
+      item.user = item.user[0]
+    })
     const total = await RecommendUserModel.estimatedDocumentCount()
     ctx.body = jsonResponse({
       data: {
         recommend, total, page: pagination.page, size: pagination.size
+      }
+    })
+  }
+
+  @GET("/random")
+  async random(ctx: IRouterContext, next: any) {
+    const fields = {
+      $project: {
+        _id: 0,
+        "user.uuid": 1,
+        "user.avatar": 1,
+        "user.name": 1,
+        "user.displayName": 1,
+        "user.bgImage": 1
+      }
+    }
+    const recommend = await RecommendUserModel.aggregate([
+      {$sample: {size: 10}},
+      {
+        $lookup: {
+          from: "users",
+          localField: "uuid",
+          foreignField: "uuid",
+          as: "user"
+        }
+      },
+      fields
+    ])
+
+    ctx.body = jsonResponse({
+      data: {
+        recommend: recommend.map(item => {
+          return item.user[0]
+        })
       }
     })
   }
