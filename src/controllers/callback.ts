fe27c394d0854bf050bf7222e96995afc83e3158
hffp <510033939@qq.com>
2020-09-30 15:00:52 +0800

    media update

--- src/controllers/callback.ts
+++ src/controllers/callback.ts
@@ -32,6 +32,7 @@ export default class CallbackController {
       const mediaInfo = mediaType(ext)
       if (data) {
         const decodedData: MediaConvertCache = JSON.parse(data);
+        console.log("callback")
         if (decodedData.fileCount > 1) {
           switch (mediaInfo.type) {
             case MEDIA_TYPE.IMAGE:
