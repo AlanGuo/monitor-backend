40fd2e2f1d47b3fdf4a6c9ea94befc6b0da2effe
hffp <510033939@qq.com>
2020-09-30 11:26:23 +0800

    media update

--- src/controllers/media.ts
+++ src/controllers/media.ts
@@ -8,6 +8,7 @@ import {redis} from "../infrastructure/redis";
 import {isVideo} from "@src/infrastructure/utils/video";
 import {isImage} from "@src/infrastructure/utils/image";
 import { getSignedUrl } from "@src/infrastructure/amazon/cloudfront";
+import {AuthRequired} from "@src/infrastructure/decorators/auth";
 
 @Controller({prefix: "/media"})
 export default class MediaController {
@@ -27,9 +28,11 @@ export default class MediaController {
   }
 
   @GET("/convert")
+  @AuthRequired()
   async convert(ctx: IRouterContext) {
     const key = decodeURIComponent(ctx.query.key);
     const ext = key.split(".")[1];
+    const uuid = ctx.state.user.uuid
     if (isVideo(ext)) {
       const fileNameWithoutExt = key.split(".")[0].replace(config.AWS_MEDIA_CONVERT.videoSourceFolder, "");
       const s3FilePath = config.AWS_MEDIA_CONVERT.sourcePath + key;
@@ -39,12 +42,14 @@ export default class MediaController {
         const decodedData = JSON.parse(data);
         decodedData.fileCount = 3;
         decodedData.key = key;
+        decodedData.owner = uuid
         await redis.set(config.AWS_MEDIA_CONVERT.videoFolder + fileNameWithoutExt, JSON.stringify(decodedData));
       } else {
         // media convertion job, three jobs
         await redis.set(config.AWS_MEDIA_CONVERT.videoFolder + fileNameWithoutExt, JSON.stringify({
           fileCount: 3,
           key,
+          owner: uuid,
           subscribers: []
         }));
       }
