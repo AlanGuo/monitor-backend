2229f0e893b274361a5c8f7a9ccc60587fac7d9b
hffp <510033939@qq.com>
2020-11-13 18:06:53 +0800

    notification update

--- src/services/consumer/index.ts
+++ src/services/consumer/index.ts
@@ -1,9 +1,11 @@
 import {loadSaveAndSendMessageConsumer} from "@src/services/consumer/saveAndSendMessage";
 import {loadSaveMediaConsumer} from "@src/services/consumer/saveMedia";
 import {loadUpdateUserSubPriceConsumer} from "@src/services/consumer/updateUserSubPrice";
+import {loadSaveNotificationConsume} from "@src/services/consumer/saveNotification";
 
 export async function loadConsumer() {
   await loadSaveAndSendMessageConsumer()
   await loadSaveMediaConsumer();
   await loadUpdateUserSubPriceConsumer();
+  await loadSaveNotificationConsume();
 }
