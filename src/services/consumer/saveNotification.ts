7b72324362a8db31caeff7c973578aefffc4b46e
hffp <510033939@qq.com>
2020-11-13 14:47:05 +0800

    update

--- src/services/consumer/saveNotification.ts
+++ src/services/consumer/saveNotification.ts
@@ -2,7 +2,7 @@ import config from "@src/infrastructure/utils/config";
 import {Consumer} from "@src/infrastructure/rabbitMq";
 import {
   JUSTFANS_EXCHANGE,
-  NOTIFICATION_ROUTING_KEY,
+  NOTIFICATION_ROUTING_KEY, NotificationType,
   RABBITMQ_EXCHANGE_TYPE,
   SAVE_NOTIFICATION_QUEUE,
 
@@ -14,7 +14,121 @@ export async function loadSaveNotificationConsume() {
   await consumer.connection(config.RABBITMQ, RABBITMQ_EXCHANGE_TYPE.DIRECT);
 
   await consumer.consume(async msg => {
-    const tmp = JSON.parse(msg);
+    const tmp: { type: NotificationType, [key: string]: any } = JSON.parse(msg);
     console.log('save notification:', msg);
+    switch (tmp.type) {
+      case NotificationType.chat:
+        await handleChat();
+        break;
+      case NotificationType.newPost:
+        await handleNewPost();
+        break;
+      case NotificationType.kycPass:
+        await handleKYCPass();
+        break;
+      case NotificationType.kycVeto:
+        await handleKYCVeto();
+        break;
+      case NotificationType.postComment:
+        await handlePostComment();
+        break;
+      case NotificationType.postLike:
+        await handlePostLike();
+        break;
+      case NotificationType.postTip:
+        await handlePostTip();
+        break;
+      case NotificationType.commentLike:
+        await handleCommentLike();
+        break;
+      case NotificationType.commentReply:
+        await handleCommentReply();
+        break;
+      case NotificationType.postPay:
+        await handlePostPay();
+        break;
+      case NotificationType.messagePay:
+        await handleMessagePay();
+        break;
+      case NotificationType.followExpired:
+        await handleFollowExpired();
+        break;
+      case NotificationType.subExpired:
+        await handleSubExpired();
+        break;
+      case NotificationType.subCancel:
+        await handleSubCancel();
+        break;
+      case NotificationType.sub:
+        await handleSub();
+        break;
+      case NotificationType.tip:
+        await handleTip();
+        break;
+    }
   })
+}
+
+async function handleChat () {
+  // TODO
+}
+
+async function handleNewPost () {
+  // TODO
+}
+
+async function handleKYCPass () {
+  // TODO
+}
+
+async function handleKYCVeto () {
+  // TODO
+}
+
+async function handlePostComment () {
+  // TODO
+}
+
+async function handlePostLike () {
+  // TODO
+}
+
+async function handlePostTip () {
+  // TODO
+}
+
+async function handleCommentLike () {
+  // TODO
+}
+
+async function handleCommentReply () {
+  // TODO
+}
+
+async function handlePostPay () {
+  // TODO
+}
+
+async function handleMessagePay () {
+  // TODO
+}
+
+async function handleFollowExpired () {
+  // TODO
+}
+
+async function handleSubExpired () {
+  // TODO
+}
+
+async function handleSubCancel () {
+  // TODO
+}
+
+async function handleSub () {
+  // TODO
+}
+
+async function handleTip () {
+  // TODO
 }
