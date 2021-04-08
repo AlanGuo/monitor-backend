7e3725494b8b12018bc476a633f0ea6959f1b77a
AlanGuo <AlanGuo>
2021-04-08 16:36:59 +0800

    fx bug

--- src/controllers/record.ts
+++ src/controllers/record.ts
@@ -6,8 +6,31 @@ import recordModel from "@src/models/record";
 import { jsonResponse } from "@src/infrastructure/utils";
 import { RESPONSE_CODE } from "@src/infrastructure/utils/constants";
 
-@Controller({prefix: "/record"})
+@Controller({prefix: "/records"})
 export default class RecordController {
+
+  @GET("/running")
+  async getRunningRecord(ctx: IRouterContext) {
+    const fields = {
+      datetime: 1,
+      symbol: 1,
+      longex: 1,
+      shortex: 1,
+      long_open_volume: 1,
+      short_open_volume: 1,
+      max_volume: 1,
+      long_open_balance: 1,
+      short_open_balance: 1,
+      long_open_price: 1,
+      short_open_price: 1,
+      long_close_balance: 1,
+      short_close_balance: 1,
+      profit: 1
+    };
+    const records = await recordModel.find({profit: {$exists: false}}, fields).sort({_id: -1}).limit(1);
+    ctx.body = jsonResponse({code: RESPONSE_CODE.NORMAL, data: records[0]});
+  }
+
   @GET("")
   @PaginationDec()
   async getRecords(ctx: IRouterContext) {
