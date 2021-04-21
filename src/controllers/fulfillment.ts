1ad697ed2f5cf38b4f8ad88a1d17e2af94441a63
AlanGuo <AlanGuo>
2021-04-21 15:13:17 +0800

    fullfillments lost

--- src/controllers/fulfillment.ts
+++ src/controllers/fulfillment.ts
@@ -2,7 +2,7 @@ import { Controller, GET, PUT } from "@src/infrastructure/decorators/koa";
 import { PaginationDec } from "@src/infrastructure/decorators/pagination";
 import { Pagination } from "@src/interface";
 import { IRouterContext } from "koa-router";
-import fullfilmentModel, { IFulfillment } from "@src/models/fulfillment";
+import fulfillmentModel, { IFulfillment } from "@src/models/fulfillment";
 import { jsonResponse } from "@src/infrastructure/utils";
 import { RESPONSE_CODE } from "@src/infrastructure/utils/constants";
 import { Types } from "mongoose";
@@ -35,14 +35,14 @@ export default class fulfillmentController {
     if (query.fulfill && query.fulfill.toLowerCase() == "false") {
       filter.$expr.$and.push({$lt: [ "$fill" , "$volume" ] });
     }
-    const fills = await fullfilmentModel.find(filter, fields).sort({ _id: -1 }).skip(pagination.offset).limit(pagination.limit);
-    const total = await fullfilmentModel.countDocuments(filter);
+    const fills = await fulfillmentModel.find(filter, fields).sort({ _id: -1 }).skip(pagination.offset).limit(pagination.limit);
+    const total = await fulfillmentModel.countDocuments(filter);
     ctx.body = jsonResponse({ code: RESPONSE_CODE.NORMAL, data: {fills, total} });
   }
 
   @GET("/task/fee/:id")
   async getTotalFee(ctx: IRouterContext) {
-    const totalFeeRecords = await fullfilmentModel.aggregate([
+    const totalFeeRecords = await fulfillmentModel.aggregate([
       {
         $group: {
           _id: Types.ObjectId(ctx.params.id),
@@ -51,5 +51,62 @@ export default class fulfillmentController {
       }
     ]);
     ctx.body = jsonResponse({ code: RESPONSE_CODE.NORMAL, data: {totalFee: totalFeeRecords[0].totalFee} });
+  }
+
+  @GET("/task/lost/:id")
+  async getTotalLose(ctx: IRouterContext) {
+    const fulfillments = await fulfillmentModel.find({
+      task_id: Types.ObjectId(ctx.params.id),
+    });
+    let totalLost = 0;
+    fulfillments.forEach((item, index) => {
+      const orderTime = new Date(item.datetime);
+      const nextItem = fulfillments[index+1];
+      if (nextItem) {
+        const nextItemOrderTime = new Date(nextItem.datetime);
+        if(orderTime.toDateString() == nextItemOrderTime.toDateString() &&
+        orderTime.getHours() == nextItemOrderTime.getHours() &&
+        orderTime.getMinutes() == nextItemOrderTime.getMinutes() &&
+        orderTime.getSeconds() == nextItemOrderTime.getSeconds()) {
+          if (item.fill != nextItem.fill) {
+            // 未完全成交
+            let findBalanceItemIndex = 2;
+            while(fulfillments[index + findBalanceItemIndex]) {
+              const balanceItem = fulfillments[index + findBalanceItemIndex];
+              if (balanceItem.fill > 0) {
+                // nulfillItem 不会为空，因为这里确定有两边没有同等成交
+                let nulfillItem = null;
+                if (item.fill < nextItem.fill) {
+                  nulfillItem = item;
+                } else {
+                  nulfillItem = nextItem;
+                }
+                if (nulfillItem) {
+                  console.info(nulfillItem.exchange + "的"+nulfillItem.side+"订单" + nulfillItem.order_id + "未完全成交, 挂单价: " + nulfillItem.price + ", 未成交量: " + (nulfillItem.volume - nulfillItem.fill));
+                  if(balanceItem.fill) {
+                    let lost = 0;
+                    if (nulfillItem.side === "buy") {
+                      lost = (nulfillItem.price - balanceItem.price) * balanceItem.fill;
+                    } else if (nulfillItem.side === "sell") {
+                      lost = (balanceItem.price - nulfillItem.price) * balanceItem.fill;
+                    }
+                    totalLost += lost;
+                    console.info("追加订单" + balanceItem.order_id+", 成交价: " + balanceItem.price + ", 成交量: "+ balanceItem.fill + ", 亏损: " + lost);
+                  }
+                }
+              } else {
+                break;
+              }
+              // 完全成交，表示两边仓位已拉平
+              if(balanceItem.fill == balanceItem.volume) {
+                break;
+              }
+              findBalanceItemIndex++;
+            }
+          }
+        }
+      }
+    });
+    ctx.body = jsonResponse({ code: RESPONSE_CODE.NORMAL, data: {totalLost} });
   }
 }
