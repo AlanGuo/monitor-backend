7cce64b8316798cf4f430f36e2aa639f2bc6f522
AlanGuo <AlanGuo>
2021-04-27 09:45:49 +0800

    fx bug

--- src/models/fulfillment.ts
+++ src/models/fulfillment.ts
@@ -17,7 +17,7 @@ export interface IFulfillment extends Document {
 
 const fulfillmentModel: Schema = new Schema({
   task_id: {type: Types.ObjectId, required},
-  datetime: {type: String, required},
+  datetime: {type: Date, required},
   symbol: {type: String, required},
   exchange: {type: String, required},
   order_id: {type: String, required},
