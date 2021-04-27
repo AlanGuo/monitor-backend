241e5c3511c2298c581e43b466048062c3060751
AlanGuo <AlanGuo>
2021-04-27 09:11:40 +0800

    fx bug

--- src/controllers/fulfillment.ts
+++ src/controllers/fulfillment.ts
@@ -70,10 +70,11 @@ export default class fulfillmentController {
     
     for(let index = 0 ;index < fulfillments.length; index +=1) {
       const item = fulfillments[index];
+      console.log(item);
       if (item &&
         // 只统计平仓的损失
-        (item.position === "long" && item.side === "sell" || 
-        item.position === "short" && item.side === "buy")
+        ((item.position === "long" && item.side === "sell") || 
+        (item.position === "short" && item.side === "buy"))
       ) {
         console.log(item);
         const orderTime = new Date(item.datetime);
