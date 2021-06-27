392fdc0498a55190854fed9170c33b7d128f776f
AlanGuo <AlanGuo>
2021-06-28 07:01:34 +0800

    fx bug

--- config/production.ts
+++ config/production.ts
@@ -24,7 +24,7 @@ module.exports = {
 
   MONGODB: {
     Name: "dev",
-    DB: "abtg-perp-prod-2021-05-27",
+    DB: "abtg-perp-prod-2021-06-28",
     Connection_String_URI: "mongodb://localhost:27017"
   },
   HOST: "https://monitor.bitapp.com"
