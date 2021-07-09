556fdccbedd6f87e42f5b9854aad886aa99b0e02
AlanGuo <AlanGuo>
2021-07-09 09:10:13 +0800

    fx bug

--- src/controllers/depth.ts
+++ src/controllers/depth.ts
@@ -102,75 +102,40 @@ export default class depthController {
     const short_ex = ctx.query.short;
     const limit = ctx.query.limit || config.DEPTH_LIMIT;
     const pct = ctx.query.pct;
-    const step = ctx.query.step;
     const total = await depthModel.find({symbol: ctx.params.symbol}).limit(limit).countDocuments();
     const targetCount = total * Number(pct);
-    const getCountByDiff = async function(diff: number) {
-      const countRes = await depthModel.aggregate([
-        {
-          $match: {
-            symbol: ctx.params.symbol
-          }
-        },
-        {$limit: limit},
-        {
-          $project:{
-            binance_ask: 1, binance_bid: 1, huobi_ask: 1, huobi_bid: 1, okex_ask: 1, okex_bid: 1, 
-            close_price_diff: { 
-              $subtract: [ `$${short_ex}_ask`, `$${long_ex}_bid` ] 
-            },
-            open_price_diff: { 
-              $subtract: [ `$${short_ex}_bid`, `$${long_ex}_ask` ] 
-            }
-          }
-        },
-        {
-          $project: {
-            _id: 0,
-            ts:1,
-            closePriceLte0: {$lte:["$close_price_diff", diff]},
-          }
-        },
-        {
-          $match: {
-            closePriceLte0: true
-          }
-        },
-        {
-          $count: "count"
-        }
-      ]);
-  
-      return countRes.length > 0 ? countRes[0].count : 0;
-    }
 
-    if (step <= 0) {
-      ctx.body = jsonResponse({ code: RESPONSE_CODE.ERROR, msg: "step must be greater than 0"});
-    } else {
-      let finalDiff = 0;
-      let countx = await getCountByDiff(finalDiff);
-      if (countx >= targetCount) {
-        // diff 每次减少一个 step，直到下一个 count < targetCount
-        do {
-          finalDiff = finalDiff - Number(step);
-          countx = await getCountByDiff(finalDiff);
-        } while(countx >= targetCount);
-        // diff = 上一个满足条件的 diff
-        finalDiff = finalDiff + Number(step);
-      } else {
-        // diff 每次增加一个 step，直到下一个 count >= targetCount
-        do {
-          finalDiff = finalDiff + Number(step);
-          countx = await getCountByDiff(finalDiff);
-        } while(countx < targetCount);
+    const countRes = await depthModel.aggregate([
+      {
+        $match: {
+          symbol: ctx.params.symbol
+        }
+      },
+      {$limit: limit},
+      {
+        $project:{
+          binance_ask: 1, binance_bid: 1, huobi_ask: 1, huobi_bid: 1, okex_ask: 1, okex_bid: 1, 
+          close_price_diff: { 
+            $subtract: [ `$${short_ex}_ask`, `$${long_ex}_bid` ] 
+          }
+        }
+      },
+      {
+        $sort: {
+          close_price_diff: 1
+        }
+      },
+      {
+        $limit: targetCount
       }
-      ctx.body = jsonResponse({ code: RESPONSE_CODE.NORMAL,
-        data: {
-          diff: finalDiff,
-          count: countx,
-          total
-        } 
-      });
-    }
+    ]);
+
+    ctx.body = jsonResponse({ code: RESPONSE_CODE.NORMAL,
+      data: {
+        diff: countRes.length > 0 ? countRes[0].close_price_diff : 0,
+        count: targetCount,
+        total
+      } 
+    });
   }
 }
