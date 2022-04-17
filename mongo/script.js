da16cf58efac9ec072b87c782d73df6781cc94f5
alanguo <ggddll123@qq.com>
2022-04-17 23:03:20 +0800

    update script

--- mongo/script.js
+++ mongo/script.js
@@ -38,3 +38,4 @@ db.fulfillments.find({exchange: "BYBIT", position: "long",side: "sell", fill:{$g
 
 db.fulfillments.update({order_id: "994f1557-1319-4eef-99b4-e412223375e2"}, {$set: {volume: 100, fill: 100, trade_avg_price: 4.6280}});
 db.fulfillments.insertOne( { "task_id" : ObjectId("624d3a959de4cc11d2dbfb15"), "datetime" : "2022-04-07T20:34:48.748587190+08:00", "exchange" : "BINANCE", "symbol" : "one", "order_id" : "-", "side" : "buy", "position" : "short", "price" : 0.14183, "volume" : 8398, "fill" : 8398, "fee" : 0.00029947, "fee_asset" : "bnb", "trade_avg_price" : 0.14087 } );
+db.records.update({"_id" : ObjectId("625837c1769eaae6aa090554")}, {$set: {price_diff_profit: 0 }})
